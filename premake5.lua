94881d631be19991664fd571b10d4cecbe0ac97a
Dávid Kunovics <kunovicsdavid@gmail.com>
2023-03-02 11:56:01 +0100

    Project setup

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+workspace "lava"
+	architecture "x86_64"
+	configurations { "Debug", "Release" }
+
+project "lava"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+
+	files {
+		"src/**.cpp",
+		"src/**.h",
+	}
+
+	targetdir "bin"
+	objdir    "obj/%{cfg.buildcfg}"
+
+	filter "configurations:Debug"
+		ignoredefaultlibraries { "MSVCRT" }
+		targetname "%{prj.name}d"
+		optimize "off"
+		symbols "on"
+
+	filter "configurations:Release"
+		optimize "on"
+		symbols "off"
